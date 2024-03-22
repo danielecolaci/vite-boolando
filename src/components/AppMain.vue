33a642cf618fcda4332eb7adbd52aa2b8109ab22
danielecolaci <danielecolaci@outlook.com>
2024-03-22 12:36:24 +0100

    Export ProductCard in own file component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,11 @@
 <script>
+import ProductCard from './ProductCard.vue'
+
 export default {
     name: 'AppMain',
+    components: {
+        ProductCard
+    },
     data() {
         return {
 
@@ -13,119 +18,9 @@ export default {
 
     <main>
         <div class="flex main-container">
-            <div class="card one">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
 
-                    <div class="boxes flex">
-                        <div class="box sconto">-50%</div>
-                        <div class="box sost">Sostenibilità</div>
-                    </div>
+            <ProductCard />
 
-                    <img src="/img/1.webp" alt="" class="first-img">
-                    <img src="/img/1b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Levi's</p>
-                    <h2>RELAXED FIT TEE UNISEX</h2>
-                    <p class="red-price">14,99 €</p>
-                    <span class="black-price">29,99 €</span>
-                </div>
-            </div>
-
-            <div class="card two">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-                    <div class="boxes flex">
-                        <div class="box sconto">-30%</div>
-                    </div>
-                    <img src="/img/2.webp" alt="" class="first-img">
-                    <img src="/img/2b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Guess</p>
-                    <h2>ROSES TEE</h2>
-                    <p class="red-price">20,99 €</p>
-                    <span class="black-price">29,99 €</span>
-                </div>
-            </div>
-
-            <div class="card three">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-                    <div class="boxes flex">
-                        <div class="box sconto">-30%</div>
-                    </div>
-                    <img src="/img/3.webp" alt="" class="first-img">
-                    <img src="/img/3b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Come Zucchero Filato</p>
-                    <h2>VOGLIA DI COLORI PASTELLO</h2>
-                    <p class="red-price">129,99 €</p>
-                    <span class="black-price">184,99 €</span>
-                </div>
-            </div>
-
-            <div class="card four">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-                    <div class="boxes flex">
-                        <div class="box sconto">-50%</div>
-                        <div class="box sost">Sostenibilità</div>
-                    </div>
-                    <img src="/img/4.webp" alt="" class="first-img">
-                    <img src="/img/4b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Levi's</p>
-                    <h2>TEE UNISEX</h2>
-                    <p class="red-price">14,99 €</p>
-                    <span class="black-price">29,99 €</span>
-                </div>
-            </div>
-
-            <div class="card five">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-
-                    <img src="/img/5.webp" alt="" class="first-img">
-                    <img src="/img/5b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Maya Deluxe</p>
-                    <h2>STRIPE BODICE</h2>
-                    <p class="red-price">99,99 €</p>
-                </div>
-            </div>
-
-            <div class="card six">
-                <div class="card-img">
-                    <div class="fav">
-                        <a href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-                    <div class="boxes flex">
-                        <div class="box sost">Sostenibilità</div>
-                    </div>
-                    <img src="/img/6.webp" alt="" class="first-img">
-                    <img src="/img/6b.webp" alt="" class="second img">
-                </div>
-                <div class="card-text">
-                    <p class="marca">Esprit</p>
-                    <h2>MAGLIONE - BLACK</h2>
-                    <p class="red-price">14,99 €</p>
-                </div>
-            </div>
         </div>
     </main>
 
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: 'ProductCard',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="card">
+        <div class="card-img">
+            <div class="fav">
+                <a href=""><i class="fa-solid fa-heart"></i></a>
+            </div>
+
+            <div class="boxes flex">
+                <div class="box red">-50%</div>
+                <div class="box green">Sostenibilità</div>
+            </div>
+
+            <img src="/img/1.webp" alt="" class="first-img">
+            <img src="/img/1b.webp" alt="">
+        </div>
+        <div class="card-text">
+            <p class="brand">Levi's</p>
+            <h2>RELAXED FIT TEE UNISEX</h2>
+            <p class="final-price">14,99 €</p>
+            <span class="old-price">29,99 €</span>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss"></style>
