a9590eba7e0c13382c7ec805efff95d306f876c6
danielecolaci <danielecolaci@outlook.com>
2024-03-26 21:27:30 +0100

    Fit Modal Style

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -3,6 +3,8 @@ export default {
     name: 'AppModal',
     props: {
         productId: Object
+    },
+    methods: {
     }
 }
 </script>
@@ -10,10 +12,22 @@ export default {
 <template>
 
     <div class="background" v-if="productId">
-        <div class="modal">
-            <h3>{{ productId.name }}</h3>
-            <h5>{{ productId.brand }}</h5>
-            <button @click="productId = null">x</button>
+        <div class="modal flex">
+            <div class="product-img flex">
+                <img :src="'/img/' + productId.frontImage" alt="">
+                <img :src="'/img/' + productId.backImage" alt="">
+            </div>
+            <div class="detail flex">
+                <div class="card-text">
+                    <p class="brand">{{ productId.brand }}</p>
+                    <h3>{{ productId.name }}</h3>
+                </div>
+                <div class="price flex">
+                    <span class="final-price">{{ productId.price }}</span>
+                    <span class="old-price">{{ productId.price }}</span>
+                </div>
+                <button @click="productId = null">&Cross;</button>
+            </div>
         </div>
     </div>
 
@@ -29,7 +43,52 @@ export default {
     padding: 20px;
     border-radius: 8px;
     z-index: 100;
-    text-align: center;
+    flex-direction: column;
+    gap: 10px;
+    width: 60%;
+    max-width: 800px;
+
+    .product-img {
+        width: 100%;
+        justify-content: space-between;
+        gap: 10px;
+
+        img {
+            width: 49%;
+        }
+    }
+
+    .detail {
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .price {
+        align-items: flex-end;
+        gap: 10px;
+
+        span {
+            font-size: 20px;
+        }
+    }
+
+    .old-price {
+        text-decoration: line-through;
+    }
+
+    button {
+        width: 40px;
+        height: 40px;
+        border: none;
+        border-radius: 16px;
+        background-color: white;
+        cursor: pointer;
+        position: absolute;
+        top: 40px;
+        right: 40px;
+        font-size: 20px;
+        font-weight: bolder;
+    }
 }
 
 .background {
@@ -41,11 +100,5 @@ export default {
     top: 0%;
     left: 0%;
     z-index: 90;
-}
-
-button {
-    border: none;
-    background-color: white;
-    cursor: pointer;
 }
 </style>
